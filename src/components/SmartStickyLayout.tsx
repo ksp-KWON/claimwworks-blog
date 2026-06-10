41ef61b2261017cfe0f58e67baacc19aa75fa1e2
ksp-KWON <ksp.claimworks@gmail.com>
2026-06-10 15:55:09 +0900

    fix: 스마트 사이드바 레이아웃 높이 계산 오류 및 푸터 침범 현상 수정 (react-sticky-box 제거 및 CSS sticky 속성으로 교체)

--- src/components/SmartStickyLayout.tsx
+++ src/components/SmartStickyLayout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { ReactNode } from "react";
-import StickyBox from "react-sticky-box";
 
 interface Props {
   mainContent: ReactNode;
@@ -10,7 +9,7 @@ interface Props {
 
 export default function SmartStickyLayout({ mainContent, sidebarContent }: Props) {
   return (
-    <div className="mx-auto w-full sm:w-[92vw] xl:w-[85vw] max-w-7xl px-0 sm:px-5 py-6 sm:py-8 flex flex-col lg:flex-row gap-6 lg:gap-8 flex-1">
+    <div className="mx-auto w-full sm:w-[92vw] xl:w-[85vw] max-w-7xl px-0 sm:px-5 py-6 sm:py-8 flex flex-col lg:flex-row lg:items-start gap-6 lg:gap-8 flex-1">
       
       {/* 본문 영역 */}
       <main className="w-full lg:w-[73%] flex-1 min-w-0 transition-all duration-300">
@@ -18,11 +17,8 @@ export default function SmartStickyLayout({ mainContent, sidebarContent }: Props
       </main>
 
       {/* 사이드바 영역 */}
-      {/* 부모 컨테이너가 flex이고 items-start가 없으므로 aside는 main과 동일한 높이로 늘어남 */}
-      <aside className="w-full lg:w-[27%] transition-all duration-300">
-        <StickyBox offsetTop={80} offsetBottom={20} className="w-full">
-          {sidebarContent}
-        </StickyBox>
+      <aside className="w-full lg:w-[27%] lg:sticky lg:top-[80px] lg:max-h-[calc(100vh-100px)] lg:overflow-y-auto custom-scrollbar transition-all duration-300">
+        {sidebarContent}
       </aside>
 
     </div>
